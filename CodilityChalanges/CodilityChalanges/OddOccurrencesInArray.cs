a9e2c5ebf22d5e68fdf28adae17be0e7c4bd1912
Ribeiro, Douglas <Douglas_Ribeiro@Dell.com>
2019-09-28 16:13:26 -0300

    array challange

---
+++ CodilityChalanges/CodilityChalanges/OddOccurrencesInArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilityChalanges
+{
+    public class OddOccurrencesInArray
+    {
+        public long solution(long[] array)
+        {
+
+            List<long> excluded = new List<long>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (i != j && !excluded.Contains(j) && !excluded.Contains(i))
+                    {
+                        if (array[i] == array[j])
+                        {
+                            excluded.Add(i);
+                            excluded.Add(j);
+                            break;
+                        }
+                    }
+                }
+            }
+
+             for (long h = 0; h < array.Length; h++)
+            {
+                if (!excluded.Contains(h))
+                {
+                    return array[h];
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ CodilityChalanges/CodilityChalangesTest/OddOccurrencesInArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using CodilityChalanges;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodilityChalangesTest
+{
+    public class OddOccurrencesInArrayTest
+    {
+        [Fact]
+        public void OddOccurrencesInArrayTest1()
+        {
+
+            OddOccurrencesInArray ooia = new OddOccurrencesInArray();
+
+            long[] array = { 9, 3, 9, 3, 9, 7, 9 };
+
+            long result = ooia.solution(array);
+
+            Assert.Equal(7, result);
+        }
+    }
+}
