e4f858becb2a379481bd476cd9b5aea90f0e7657
Ribeiro, Douglas <Douglas_Ribeiro@Dell.com>
2019-09-30 15:01:29 -0300

    new challanges

--- CodilityChalanges/CodilityChalangesTest/OddOccurrencesInArrayTest.cs
+++ CodilityChalanges/CodilityChalangesTest/OddOccurrencesInArrayTest.cs
@@ -11,13 +11,10 @@ namespace CodilityChalangesTest
         [Fact]
         public void OddOccurrencesInArrayTest1()
         {
-
             OddOccurrencesInArray ooia = new OddOccurrencesInArray();
 
             long[] array = { 9, 3, 9, 3, 9, 7, 9 };
-
             long result = ooia.solution(array);
-
             Assert.Equal(7, result);
         }
     }
